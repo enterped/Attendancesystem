3c148948cfa95198df0b6655bc0762897f4c7766
unknown <eduardojimenezsuero@gmail.com>
2024-03-30 00:11:39 -0600

    changed

--- src/attendappn/AttendAppn.java
+++ src/attendappn/AttendAppn.java
@@ -14,7 +14,7 @@ public class AttendAppn {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Eduardo");
+        System.out.println("Edudo");
     }
     
 }
